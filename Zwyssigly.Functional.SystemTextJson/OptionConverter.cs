d2d4359f1e3080c550bcdd6e61308a4c535b2c9c
Zwyssigly <>
2020-08-28 15:26:05 +0200

    add system.text.json converter

---
+++ Zwyssigly.Functional.SystemTextJson/OptionConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Zwyssigly.Functional.SystemTextJson
+{
+    public class OptionConverter<T> : JsonConverter<Option<T>>
+    {
+        public override Option<T> Read(ref Utf8JsonReader reader, System.Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Null) {
+                reader.Read();
+                return Option.None<T>();
+            }
+
+            return Option.Some(((JsonConverter<T>)options.GetConverter(typeof(T))).Read(ref reader, typeof(T), options));
+        }
+
+        public override void Write(Utf8JsonWriter writer, Option<T> value, JsonSerializerOptions options)
+        {
+            value.Match(
+                some => ((JsonConverter<T>)options.GetConverter(typeof(T))).Write(writer, some, options),
+                () => writer.WriteNullValue()
+            );
+        }
+    }
+}
---
+++ Zwyssigly.Functional.SystemTextJson/OptionConverterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Zwyssigly.Functional.SystemTextJson
+{
+    public class OptionConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert.IsGenericType && typeToConvert.GetGenericTypeDefinition() == typeof(Option<>);
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            return (JsonConverter) Activator.CreateInstance(typeof(OptionConverter<>).MakeGenericType(typeToConvert.GetGenericArguments()[0]));
+        }
+    }
+}
